9cf09027e78012a1dc782acfb4cfcbd0caa19b89
Sapir Malka <itssapir@gmail.com>
2020-01-13 16:00:26 +0200

    added node helpers implementations

--- part3_helpers.cpp
+++ part3_helpers.cpp
@@ -4,15 +4,18 @@
 #include "part3_helpers.hpp"
 
 void CodeBuffer::emit(string code) {
-
+    this->code.push_back(code);
 }
 
 void CodeBuffer::backPatch(vector<int>& list, int quad) {
-
+    for( int i=0; i< list.size; ++i) {
+        int codeLineToChange = list[i];
+        this->code[codeLineToChange] += " " + intToString(quad);
+    }
 }
 
 int CodeBuffer::nextQuad() {
-
+    return this->code.size();
 }
 
  
