eb964fb07407acdc18f3c8458ba51fa55ef8d7cb
Sapir Malka <itssapir@gmail.com>
2020-01-18 19:07:03 +0200

    Organized and documented tests, added tests for while loops and a couple more

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,19 @@
 import os
+import glob
 import re
 
 def compile():
     os.system("make")
-    os.system("rm tests/*\.rsk")
-    for fileName in os.listdir("tests"):
-        if(fileName.endswith(".cmm")):
-            print("Compiling " + fileName)
-            os.system("./rx-cc tests/" + fileName)
+    # compile success examples
+    for fileName in sorted(glob.glob("./tests/example*.cmm"), key = lambda x: (int(re.sub('\D','',x)),x)):
+        print("Compiling " + fileName)
+        os.system("./rx-cc " + fileName)
+    print("")
+    # compile fail examples
+    for fileName in sorted(glob.glob("./tests/fail*.cmm"), key = lambda x: (int(re.sub('\D','',x)),x)):
+        print("Compiling " + fileName)
+        os.system("./rx-cc " + fileName)
+    print("")
     n = 1
     while(True):
         isTestNFile = re.compile("[^0-9]*" + str(n) + "[^0-9]*\.rsk")
@@ -24,14 +30,18 @@ def compile():
 
 def runTests(n = -1):
     if (n == -1):
-        for fileName in os.listdir("tests"):
-            if(fileName.endswith(".e")):
-                print("\nRunning " + fileName)
-                os.system("./rx-vm1.1 tests/" + fileName)
+        for fileName in sorted(glob.glob("./tests/*.e"), key = lambda x: (int(re.sub('\D','',x)),x)):
+            print("\nRunning " + fileName)
+            os.system("./rx-vm1.1 " + fileName)
     else:
         os.system("./rx-vm1.1 tests/out" + str(n) + ".e")
 
+def clearCompiledFiles():
+    os.system("rm tests/*\.rsk")
+    os.system("rm tests/*\.e")
+
 if __name__ == "__main__":
+    clearCompiledFiles()
     compile()
     n = raw_input("Run executables? y/n (enter test # for a specific test)").lower()
     if (n == "y"):
